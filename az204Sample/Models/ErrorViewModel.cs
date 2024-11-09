3abd3d2bc387f1eb62a254b6dc46b624518bf4c2
jpedro1711 <joaopedroigeski@gmail.com>
2024-11-09 14:24:41 -0300

    add files

---
+++ az204Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace az204Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
